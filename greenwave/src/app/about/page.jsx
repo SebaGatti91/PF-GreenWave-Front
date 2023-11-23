f8232aa80e0c4e759e02921d924804fab26b7e8f
MBuenoMartinez <martin.bueno10@hotmail.com>
2023-11-23 18:22:19 -0300

    about y detail funcionando

--- greenwave/src/app/about/page.jsx
+++ greenwave/src/app/about/page.jsx
@@ -1,7 +1,56 @@
+import data from "./team.json";
+import Image from "next/image";
 const About = () => {
   return (
     <div>
-      <h1>Hola soy About</h1>
+      <Image
+        src="/images/about.png"
+        alt="mision-visionImage"
+        width={1000}
+        height={800}
+      />
+      <h1>About team</h1>
+      {data.map((member) => (
+        <div>
+          <Image
+            src={member.image ? member.image : "/images/logo.png"}
+            alt={member.lastName}
+            width={300}
+            height={300}
+          />
+          <h4>
+            {member.name} {member.lastName}
+          </h4>
+          <h4>{member.profession}</h4>
+          <a href={member.socialMedia.linkedin} target="_blank">
+            <Image
+              src="/images/linkedin.png"
+              alt="linkedinImage"
+              width={40}
+              height={40}
+              style={{ backgroundColor: "#ffffff" }}
+            />
+          </a>
+          <a href={member.socialMedia.github} target="_blank">
+            <Image
+              src="/images/github.png"
+              alt="githubImage"
+              width={40}
+              height={40}
+              style={{ backgroundColor: "#ffffff" }}
+            />
+          </a>
+          <a href={member.socialMedia.instagram} target="_blank">
+            <Image
+              src="/images/instagram.png"
+              alt="instagramImage"
+              width={40}
+              height={40}
+              style={{ backgroundColor: "#ffffff" }}
+            />
+          </a>
+        </div>
+      ))}
     </div>
   );
 };
--- greenwave/src/app/store/[id]/page.jsx
+++ greenwave/src/app/store/[id]/page.jsx
@@ -1,5 +1,23 @@
-export default function Detail(){
-    return(
-        <h1>Detail</h1>
-    )
+"use client";
+import products from "./products.json";
+import { useParams } from "next/navigation";
+import Image from "next/image";
+export default function Detail() {
+  const params = useParams();
+  const { id } = params;
+  // Encuentra el producto correspondiente en base al ID
+  const product = products.find((product) => product.id === id);
+
+  if (!product) {
+    return <p>Producto no encontrado</p>;
+  }
+
+  return (
+    <div>
+      <h3>{product.name}</h3>
+      <Image src={product.img} alt={product.name} width={500} height={500} />
+      <p>Precio: ${product.price}</p>
+      <p>{product.description}</p>
+    </div>
+  );
 }
