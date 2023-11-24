cf5f27669a55f469f3bbd4c21092129a343af2ed
D Omar Florez <sneiderflorez2002@gmail.com>
2023-11-24 07:58:04 -0500

    .

--- greenwave/next.config.js
+++ greenwave/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: { 
+    domains: ["i.imgur.com","i.pinimg.com", "th.bing.com", "previews.123rf.com", "img.freepik.com", "ar.pinterest.com"],
+},
+    pageExtensions: ["js", "jsx", "ts", "tsx", "mp4"],
+}
+
 
 module.exports = nextConfig
