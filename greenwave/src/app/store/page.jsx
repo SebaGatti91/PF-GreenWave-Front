d0839217eb07d490eb9e4c30b4c56a1b56217642
MBuenoMartinez <martin.bueno10@hotmail.com>
2023-11-26 15:48:56 -0300

    funcionalidad de filtros y ordenamientos separados

--- greenwave/src/app/store/page.jsx
+++ greenwave/src/app/store/page.jsx
@@ -1,38 +1,69 @@
-'use client'
+"use client";
 import Card from "../components/card/Card";
 import axios from "axios";
-import { useState } from 'react'
-import Pagination from '../components/pagination/Pagination'
+import { useState, useEffect } from "react";
+import Pagination from "../components/pagination/Pagination";
 
-const Store = async () => {
+const Store = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [products, setProducts] = useState([]);
+  const productsPerPage = 6;
 
-  const [currentPage, setCurrentPage] = useState(1)
-  const productsPerPage = 6
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/store");
+        const { data } = response;
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  const loadProducts = async () => {
-    try {
-      const response = await axios.get("http://localhost:3001/store");
-      const { data } = response;
-      return data;
-    } catch (error) {
-      throw Error(error);
-    }
-  };
-
-  const products = await loadProducts();
+    loadProducts();
+  }, []);
 
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
 
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
 
+  const handleOrder = async (event) => {
+    try {
+      const { value } = event.target;
+      const response = await axios.get(
+        `http://localhost:3001/store?sort=${value}`
+      );
+      const { data } = response;
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleFilter = async (event) => {
+    try {
+      const { value } = event.target;
+      const response = await axios.get(
+        `http://localhost:3001/store?filter=${value}`
+      );
+      const { data } = response;
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-evenly mb-4">
-        <div className="mr-4">
+        {/* <div className="mr-4">
           <select
             className="py-1 px-2 bg-hover hover:bg-boton hover:cursor-pointer"
             style={{ borderRadius: "1em 1em" }}
@@ -47,75 +78,96 @@ const Store = async () => {
               Wood
             </option>
           </select>
-        </div>
+        </div> */}
 
         <div className="mr-4">
           <select
+            onChange={handleFilter}
             className="py-1 px-2 bg-hover hover:bg-boton hover:cursor-pointer"
             style={{ borderRadius: "1em 1em" }}
           >
             <option className="text-center" value="Products">
               Rating
             </option>
-            <option className="text-center" value="Rating">
-              Ascendent
+            <option className="text-center" value="1">
+              1
             </option>
-            <option className="text-center" value="Materials">
-              Descendent
+            <option className="text-center" value="2">
+              2
+            </option>
+            <option className="text-center" value="3">
+              3
+            </option>
+            <option className="text-center" value="4">
+              4
+            </option>
+            <option className="text-center" value="5">
+              5
             </option>
           </select>
         </div>
         <div className="mr-4">
           <select
+            onChange={handleOrder}
             className="py-1 px-2 bg-hover hover:bg-boton hover:cursor-pointer"
             style={{ borderRadius: "1em 1em" }}
           >
             <option className="text-center" value="Alfabetico">
-              Order
+              Order Alfabetic
             </option>
-            <option className="text-center" value="Ascendent">
+            <option className="text-center" value="nameAsc">
               Ascendent
             </option>
-            <option className="text-center" value="Descendent">
+            <option className="text-center" value="nameDesc">
               Descendent
             </option>
           </select>
         </div>
         <div>
           <select
+            onChange={handleOrder}
             className="py-1 px-2 bg-hover hover:bg-boton hover:cursor-pointer"
             style={{ borderRadius: "1em 1em" }}
           >
             <option className="text-center" value="Price">
               Price
             </option>
-            <option className="text-center" value="Ascendent">
+            <option className="text-center" value="priceAsc">
               Ascendent
             </option>
-            <option className="text-center" value="Descendent">
+            <option className="text-center" value="priceDesc">
               Descendent
             </option>
           </select>
         </div>
       </div>
 
-      {products.length ? <div>
-        <Pagination
-          currentPage={currentPage}
-          totalPages={Math.ceil(products.length / productsPerPage)}
-          onPageChange={paginate}
-        />
-        </div> : <div><p> No existen coincidencias entre los filtros aplicados.</p></div>
-      }
+      {products.length ? (
+        <div>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={Math.ceil(products.length / productsPerPage)}
+            onPageChange={paginate}
+          />
+        </div>
+      ) : (
+        <div>
+          <p> No existen coincidencias entre los filtros aplicados.</p>
+        </div>
+      )}
 
       <div className="flex flex-wrap justify-center items-center mb-3">
         {currentProducts.map((product) => (
-          <div key={product.id} className=" hover:transform hover:scale-105 transition-transform duration-300">
+          <div
+            key={product.id}
+            className=" hover:transform hover:scale-105 transition-transform duration-300"
+          >
             <Card
               id={product.id}
               name={product.name}
               img={product.img}
               price={product.price}
+              rating={product.rating}
             />
           </div>
         ))}
