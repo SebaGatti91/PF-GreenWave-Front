96c216c6d698a9d28818c6892479c7e0045f4b22
D Omar Florez <sneiderflorez2002@gmail.com>
2023-11-28 16:56:23 -0500

    carrito de compras

--- greenwave/src/app/components/card/Card.jsx
+++ greenwave/src/app/components/card/Card.jsx
@@ -2,23 +2,24 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import {useState } from "react";
-import { CartContext } from './CartContext';
+import {useState} from "react";
+import { useCart } from "../cart/cartContext";
 
-
+// funcion remover carrito removeFromCart
+//funcion setear carrito clearCart
 const Card = ({
   id,
   name,
   image,
   price,
   rating,
-  
-  showStars = true,
-  showbuybutton = true,
+  cartControlers=false
+ 
 }) => {
   const [fav, setFav] = useState(false);
-  const [cart, setCart] = useContext(CartContext);
-  
+const { cart, addToCart, removeFromCart, countDownCart, countUpCart} =useCart()
+  const count = cart[0].count
+
 
   const handleFavorite = () => {
     if (fav) {
@@ -27,6 +28,7 @@ const Card = ({
       setFav(true);
     }
   };
+
 
   return (
     <div className="bg-white shadow-2xl rounded-md m-3 max-w-xs flex flex-col relative">
@@ -37,6 +39,11 @@ const Card = ({
           <button onClick={handleFavorite}>🤍</button>
         )}
       </div>
+      {cartControlers && (
+        <div className="absolute top-0 left-0 m-2">
+          <button onClick={()=>{removeFromCart(id)}} >❎</button>
+        </div>
+      )}
 
       <div className="flex-grow flex-shrink-0">
         <Image
@@ -53,16 +60,27 @@ const Card = ({
         <h3 className="text-green-600  text-center">USD {price}</h3>
         <p>rating: {rating}</p>
         <div>
-          {showStars && (
+         
             <div className="flex justify-center items-center">
               ⭐️⭐️⭐️⭐️⭐️
             </div>
-          )}
-          {showbuybutton && (
-            <div>
-              <button onClick={addToCart} className="p-1 m-2 rounded-lg bg-yellow-500">
+          {cartControlers ? (
+            <div>  
+              <div className="flex justify-center items-center">        
+              <h3>Qty:{count}</h3>
+              </div>
+            <div className="flex justify-center items-center">
+              <div >
+              <button onClick={()=>{countDownCart(id)}} style={{display: count > 0 ? 'block' : 'none'}}>-1</button>
+            
+              </div>
+              <div className="flex justify-center items-center">
+              <div>
+              <button onClick={() => addToCart({ id, name, image, price, rating })} className="p-1 m-2 rounded-lg bg-yellow-500">
                 Add to Cart
               </button>
+              
+              
               <Link href={`/store/${id}`}>
                 <button
                   className="p-1 bg-yellow-500 rounded-full "
@@ -71,8 +89,32 @@ const Card = ({
                   🔍
                 </button>
               </Link>
+              </div>
+              </div>
+              <div>
+              <button onClick={()=>{countUpCart(id)}}>+1</button>
+              </div>
+            </div>
+            </div>  
+          ):(
+            <div>
+              
+              <button onClick={() => addToCart({ id, name, image, price, rating })} className="p-1 m-2 rounded-lg bg-yellow-500">
+                Add to Cart
+             </button>
+             
+              <Link href={`/store/${id}`}>
+                <button
+                  className="p-1 bg-yellow-500 rounded-full "
+                  style={{ backgroundColor: "rgba(170, 180, 154, 0.5)" }}
+                >
+                  🔍
+                </button>
+              </Link>
+             
             </div>
           )}
+          
         </div>
       </div>
     </div>
--- greenwave/src/app/store/buycar/page.jsx
+++ greenwave/src/app/store/buycar/page.jsx
@@ -1,14 +1,31 @@
-import React, { useContext } from 'react';
-import { CartContext } from './CartContext';
+"use client";
+import React, { useContext, useEffect } from "react";
+import { CartContext } from "../../components/cart/cartContext";
+import Card from "../../components/card/Card";
 
 const Cart = () => {
-  const [cart] = useContext(CartContext);
+  const { cart } = useContext(CartContext);
+  useEffect(() => {
+    console.log(cart);
+  });
 
   return (
     <div>
+        <h1 className="flex justify-center items-center bold" > Cart Items</h1>
+        <div className="flex flex-col w-1/3 m-12 " >
       {cart.map((product, index) => (
-        <h2 key={index}>{product.name}</h2>
+        <div id={index} className="">
+          <Card
+            id={product.id}
+            name={product.name}
+            image={product.image}
+            price={product.price}
+            rating={product.rating}
+            cartControlers={true}
+          />
+        </div>
       ))}
+    </div>
     </div>
   );
 };
