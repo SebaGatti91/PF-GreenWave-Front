1e5f76e59884314fba4b62f287505ae584b3ea34
SebaGatti <seba.g.1391@gmail.com>
2023-12-03 17:23:42 -0300

    agregado products dashboard

---
+++ greenwave/src/app/banned/page.jsx
@@ -0,0 +1,13 @@
+const Banned = () => {
+  return (
+    <div>
+      <h1>Baneado</h1>
+      <p>
+        Lo sentimos, pero no puedes acceder a esta página porque has sido
+        baneado.
+      </p>
+    </div>
+  );
+};
+
+export default Banned;
--- greenwave/src/app/components/users/globalUsers.jsx
+++ greenwave/src/app/components/users/globalUsers.jsx
@@ -1,31 +1,8 @@
 "use client";
-import React, { createContext, useContext, useEffect, useState } from "react";
-import axios from "axios";
-import { useSession } from "next-auth/react";
+import React, { createContext, useContext,useState } from "react";
 export const GlobalUser = createContext();
 export const UserGlobal = ({ children }) => {
-  const { data: session } = useSession();
-  const userData = session?.user;
   const [user, setUser] = useState({});
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:3001/users/${userData?.email}`
-      );
-      const { data } = response;
-
-      setUser(data);
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-      // Handle the error, show a message to the user, or redirect as needed.
-    }
-  };
-
-  useEffect(() => {
-    if (session?.user) {
-      fetchData();
-    }
-  }, [session?.user]);
 
   return (
     <GlobalUser.Provider
