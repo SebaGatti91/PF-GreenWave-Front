77ff02e73edf91107902e0b22934b4b6cc6efc0f
ulminadir <ulminadir99@gmail.com>
2023-12-03 22:46:05 -0300

    se empieza a estilar ruta successfully

--- greenwave/src/app/successfully/page.jsx
+++ greenwave/src/app/successfully/page.jsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
 
-export default function Sucess(){
-    return(
-        <div className="m-3 flex flex-col justify-center items-center" style={{height: "69.5vh"}}>
-            <h1 className="text-3xl font-bold text-center">Your purchase has been a success!</h1>
-            <span>Thank you for choosing us</span>
-            <Link href="/homepage">
-            <button>Back to Home</button>
-            </Link>
-        </div>
-    )
+export default function Sucess() {
+  return (
+    <div
+      className="m-3 flex flex-col justify-center items-center"
+      style={{ height: "69.5vh" }}
+    >
+      <h1 className="text-3xl font-bold text-center">
+        Your purchase has been a success!
+      </h1>
+      <span className="m-3 block max-w-lg">
+        Thank you from the bottom of our hearts for choosing us! Your purchase
+        has been a resounding success, and we are thrilled to have you as part
+        of our valued customer community.
+      </span>
+      <Link href="/homepage">
+        <button className="m-3 bg-transparent border border-black text-black hover:bg-green-600 hover:text-white px-4 py-2 rounded">
+          Back to Home
+        </button>
+      </Link>
+    </div>
+  );
 }
