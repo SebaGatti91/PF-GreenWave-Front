8b11487c101261d34a9f07df5050e38ac3aceba0
D Omar Florez <sneiderflorez2002@gmail.com>
2023-12-06 13:39:02 -0500

    websocket para comunicacion continua

--- greenwave/src/app/Providers.jsx
+++ greenwave/src/app/Providers.jsx
@@ -3,12 +3,15 @@
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "./components/cart/cartContext";
 import { UserGlobal } from "./components/users/globalUsers";
+import {SocketProvider} from './websocket'
 export function Providers({ children }) {
   return (
     <SessionProvider>
+      <SocketProvider>
       <CartProvider>
         <UserGlobal>{children}</UserGlobal>
       </CartProvider>
+      </SocketProvider>
     </SessionProvider>
   );
 }
