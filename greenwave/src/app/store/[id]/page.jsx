3333864d5fe4f54dccaa2d9732d6c150f294d5f9
ulminadir <ulminadir99@gmail.com>
2023-12-10 23:31:15 -0300

    se modifica detail

--- greenwave/src/app/store/[id]/page.jsx
+++ greenwave/src/app/store/[id]/page.jsx
@@ -54,7 +54,7 @@ export default function Detail({ params }) {
   }, []);
 
   const [activeImg, setActiveImg] = useState(null);
- 
+
   const createPreference = async () => {
     try {
       const response = await axios.post(`${BackUrl}/mercadoPago`, {
@@ -130,181 +130,199 @@ export default function Detail({ params }) {
     return (
       <div>
         <div className="flex flex-col justify-between lg:flex-row md:justify-center md:items-center p-6 gap-12">
-        <div className="flex flex-col gap-5 lg:w-4/12 md:w-2/4 cursor-pointer">
-        <Link href="/store">
+          <div className="flex flex-col gap-5 lg:w-4/12 md:w-2/4 cursor-pointer">
+            <Link href="/store">
               <button
                 className="bg-transparent text-gray-600"
                 style={{ borderRadius: "2em 2em" }}
               >
-                <span className="mr-2 text-gray-600 ">&#8592;</span> BACK TO STORE
+                <span className="mr-2 text-gray-600 ">&#8592;</span> BACK TO
+                STORE
               </button>
             </Link>
-          <img
-            src={activeImg || product?.image[0]}
-            alt=""
-            className="w-full h-full aspect-square object-cover rounded-xl"
-          />
-          <div className="flex flex-row justify-between h-24">
             <img
-              src={product?.image[0]}
+              src={activeImg || product?.image[0]}
               alt=""
-              className="w-20 h-24 lg:w-28 lg:h-28 rounded-md cursor-pointer"
-              onClick={() => setActiveImg(product?.image[0])}
+              className="w-full h-full aspect-square object-cover rounded-xl"
             />
-            <img
-              src={product?.image[1]}
-              alt=""
-              className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
-              onClick={() => setActiveImg(product?.image[1])}
-            />
-            <img
-              src={product?.image[2]}
-              alt=""
-              className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
-              onClick={() => setActiveImg(product?.image[2])}
-            />
-            <img
-              src={product?.image[3]}
-              alt=""
-              className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
-              onClick={() => setActiveImg(product?.image[3])}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col gap-4">
-          {user.admin || foundUserProduct ? (
-            <div className="flex justify-end">
-              <button
-                onClick={handleProd}
-                className="bg-orange-900 hover:bg-red-700 text-white font-bold m-3 px-4 py-1 rounded"
-                style={{
-                  borderRadius: "2em 2em",
-                 
-                }}
-              >
-                <img
-                  src="/images/borrar.png"
-                  alt="borrado"
-                  style={{ width: "22px", height: "22px" }}
-                />
-              </button>
-              <button
-                onClick={handleEdit}
-                className="bg-hover hover:bg-blue-700 text-white font-bold m-3 px-4 py-1 rounded"
-                style={{
-                  borderRadius: "2em 2em",
-                  background:
-                    "linear-gradient(to right top, #445a59, #446463, #446e6d, #427978, #408383)",
-                }}
-              >
-                <img
-                  src="/images/editar.png"
-                  alt="borrado"
-                  style={{ width: "22px", height: "22px" }}
-                />
-              </button>
+            <div className="flex flex-row justify-between h-24">
+              <img
+                src={product?.image[0]}
+                alt=""
+                className="w-20 h-24 lg:w-28 lg:h-28 rounded-md cursor-pointer"
+                onClick={() => setActiveImg(product?.image[0])}
+              />
+              <img
+                src={product?.image[1]}
+                alt=""
+                className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
+                onClick={() => setActiveImg(product?.image[1])}
+              />
+              <img
+                src={product?.image[2]}
+                alt=""
+                className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
+                onClick={() => setActiveImg(product?.image[2])}
+              />
+              <img
+                src={product?.image[3]}
+                alt=""
+                className="w-20 h-24 lg:w-28 lg:h-28 md:w-14 rounded-md cursor-pointer"
+                onClick={() => setActiveImg(product?.image[3])}
+              />
             </div>
-          ) : (
-            ""
-          )}
-          <div>
-            <span className="text-green-700 font-semibold">
-              {product.materials}
-            </span>
-            <h1 className="text-3xl font-bold">{product.name}</h1>
           </div>
-          <p className="text-gray-600">{product.description}</p>
-          <h4 className="text-lg font-semibold">${product.price}</h4>
-          <p>Stock: {product.stock}</p>
-          <div>
-            <div className="flex flex-col items-center text-center p-3 ml-3">
-              <div>
-                <div className="flex justify-center items-center">
-                  <button
-                    onClick={handleBuy}
-                    className="bg-green-800 text-white font-semibold m-2 p-1 px-16 rounded-md h-full"
-                  >
-                    Buy now
-                  </button>
-
-                  <div className="flex justify-start items-start">
-                    {addedToCart ? (
-                      <>
-                        <button
-                          className="bg-orange-900 hover:bg-red-700 p-1 rounded-md"
-                          onClick={() => handleRemoveFromCart()}
-                        >
-                          {
-                            <img
-                              src="/images/rubishBeen.png"
-                              alt="rubishBeen"
-                              className="w-7 h-7"
-                            />
-                          }
-                        </button>
-                        <button
-                          className="px-3 py-1 ml-4"
-                          onClick={() => countDownCart(params.id)}
-                          style={{
-                            display: item && item.count > 0 ? "block" : "none",
-                            border: "1px solid gray",
-                          }}
-                        >
-                          -
-                        </button>
-                        <h3 className="bg-hover hover:bg-boton px-3 py-1">
-                          {item ? item.count : 0}
-                        </h3>
-                        <button
-                          className="px-3 py-1"
-                          onClick={() => countUpCart(params.id, product.stock)}
-                          style={{ border: "1px solid gray" }}
-                        >
-                          +
-                        </button>
-                      </>
-                    ) : (
-                      <div className="flex flex-row items-center">
-                        <button
-                          className="hover:text-green-900 bg-transparent text-black px-3 py-1 rounded-md border border-solid border-gray-500 hover:border-green-900"
-                          onClick={handleAddToCart}
-                        >
-                          🛒 Add to cart
-                        </button>
-                      </div>
-                    )}
-                  </div>
-                </div>
+          <div className="flex flex-col gap-4">
+            {user.admin || foundUserProduct ? (
+              <div className="flex justify-end">
+                <button
+                  onClick={handleProd}
+                  className="bg-orange-900 hover:bg-red-700 text-white font-bold m-3 px-4 py-1 rounded"
+                  style={{
+                    borderRadius: "2em 2em",
+                  }}
+                >
+                  <img
+                    src="/images/borrar.png"
+                    alt="borrado"
+                    style={{ width: "22px", height: "22px" }}
+                  />
+                </button>
+                <button
+                  onClick={handleEdit}
+                  className="bg-hover hover:bg-blue-700 text-white font-bold m-3 px-4 py-1 rounded"
+                  style={{
+                    borderRadius: "2em 2em",
+                    background:
+                      "linear-gradient(to right top, #445a59, #446463, #446e6d, #427978, #408383)",
+                  }}
+                >
+                  <img
+                    src="/images/editar.png"
+                    alt="borrado"
+                    style={{ width: "22px", height: "22px" }}
+                  />
+                </button>
               </div>
-              {/* Modal de edición */}
-              {isEditing && (
-                <div className="fixed top-0 bottom-0 w-9/12 flex items-center justify-center m-4	">
-                  <div className="bg-white p-3 rounded-lg">
-                    <PostProduct initialValues={product} isOff={false} />
+            ) : (
+              ""
+            )}
+            <div>
+              <span className="text-green-700 font-semibold">
+                {product.materials}
+              </span>
+              <h1 className="text-3xl font-bold">{product.name}</h1>
+            </div>
+            <p className="text-gray-600">{product.description}</p>
+            <h4 className="text-lg font-semibold">${product.price}</h4>
+            <p
+              className={`m-2 rounded-lg ${
+                product.stock > 0 ? "bg-green-500" : "bg-red-500"
+              }`}
+              style={{ border: "1px solid #718096" }}
+            >
+              Stock: {product.stock}
+            </p>
 
-                    <div className="flex justify-center">
+            <div>
+              <div className="flex flex-col items-center text-center p-3 ml-3">
+                {product.stock !== 0 &&
+                product.paused !== true &&
+                !foundUserProduct ? (
+                  <div>
+                    <div className="flex justify-center items-center">
                       <button
-                        className=" bg-orange-800 hover:bg-red-700 text-white py-2 px-4 rounded"
-                        onClick={closeModal}
+                        onClick={handleBuy}
+                        className="bg-green-800 text-white font-semibold m-2 p-1 px-16 rounded-md h-full"
                       >
-                        Close
+                        Buy now
                       </button>
+
+                      <div className="flex justify-start items-start">
+                        {addedToCart ? (
+                          <>
+                            <button
+                              className="bg-orange-900 hover:bg-red-700 p-1 rounded-md"
+                              onClick={() => handleRemoveFromCart()}
+                            >
+                              {
+                                <img
+                                  src="/images/rubishBeen.png"
+                                  alt="rubishBeen"
+                                  className="w-7 h-7"
+                                />
+                              }
+                            </button>
+                            <button
+                              className="px-3 py-1 ml-4"
+                              onClick={() => countDownCart(params.id)}
+                              style={{
+                                display:
+                                  item && item.count > 0 ? "block" : "none",
+                                border: "1px solid gray",
+                              }}
+                            >
+                              -
+                            </button>
+                            <h3 className="bg-hover hover:bg-boton px-3 py-1">
+                              {item ? item.count : 0}
+                            </h3>
+                            <button
+                              className="px-3 py-1"
+                              onClick={() =>
+                                countUpCart(params.id, product.stock)
+                              }
+                              style={{ border: "1px solid gray" }}
+                            >
+                              +
+                            </button>
+                          </>
+                        ) : (
+                          <div className="flex flex-row items-center">
+                            <button
+                              className="hover:text-green-900 bg-transparent text-black px-3 py-1 rounded-md border border-solid border-gray-500 hover:border-green-900"
+                              onClick={handleAddToCart}
+                            >
+                              🛒 Add to cart
+                            </button>
+                          </div>
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              )}
+                ) : (
+                  ""
+                )}
+
+                {/* Modal de edición */}
+                {isEditing && (
+                  <div className="fixed top-0 bottom-0 w-9/12 flex items-center justify-center m-4	">
+                    <div className="bg-white p-3 rounded-lg">
+                      <PostProduct initialValues={product} isOff={false} />
+
+                      <div className="flex justify-center">
+                        <button
+                          className=" bg-orange-800 hover:bg-red-700 text-white py-2 px-4 rounded"
+                          onClick={closeModal}
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
-      <div className="flex">
-          <ReviewList rating={product.rating} reviewedBy={product.Reviews} />
-          {/* <button onClick={handlepost} className="elemento">
+        <div>
+          <div className="flex">
+            <ReviewList rating={product.rating} reviewedBy={product.Reviews} />
+            {/* <button onClick={handlepost} className="elemento">
             add a review
           </button> */}
+          </div>
         </div>
-      </div>
       </div>
     );
   }
