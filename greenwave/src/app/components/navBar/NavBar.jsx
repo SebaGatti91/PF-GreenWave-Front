d7897748e35318136d334593d33a9d29bd238ba1
MLucasDelgado <lucasdelgado1011@gmail.com>
2023-12-11 14:44:57 -0300

    cambio de estructura

--- greenwave/src/app/components/navBar/NavBar.jsx
+++ greenwave/src/app/components/navBar/NavBar.jsx
@@ -5,13 +5,15 @@ import { usePathname } from "next/navigation";
 import ButtonAuth from "../buttonAuth/ButtonAuth";
 import { useSession } from "next-auth/react";
 import { CartContext } from "../cart/cartContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Image from "next/image";
 import { GlobalUser } from "../users/globalUsers";
+import './NavBar.css'
+
 const LogoSection = () => {
   const { user } = useContext(GlobalUser);
   return (
-    <div className="flex items-center flex-row gap-10">
+    <div className="flex items-center flex-row gap-10 logo">
       <Link className="" href="/">
         <img
           className="ml-2"
@@ -21,7 +23,7 @@ const LogoSection = () => {
         />
       </Link>
 
-      <Link href={"/profile"} className=" flex flex-row gap-5">
+      <Link href={"/profile"} className="direccion flex flex-row gap-5">
         <Image
           src={"/images/ubicacion.png"}
           alt={"ubicacionImage"}
@@ -40,96 +42,109 @@ const SearchSection = ({ pathname }) => (
   </section>
 );
 
-const NavigationLinks = ({ session, totalItems }) => (
-  <section className="flex gap-10 items-center">
-    <Link
-      className="text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300 "
-      style={{
-        fontFamily: "font-serif",
-        ":hover": {
-          background:
-            "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
-        },
-      }}
-      href="/homepage"
-    >
-      Home
-    </Link>
+const NavigationLinks = ({ session, totalItems, isMenuOpen, toggleMenu }) => {
+  return (
+    <section id="contenedor-menu" className={`flex gap-10 items-center ${isMenuOpen ? 'menu-open' : 'menu-closed'}`}>
+      <img className="menu" src="/images/burger_menu.png" alt="menu abierto" id="abrir" onClick={toggleMenu} />
+      <img className="menu" src="/images/cerrar_menu.png" alt="menu cerrado" id="cerrar" onClick={toggleMenu} />
+     <div className="enlaces">
+     <Link
+        className="link text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300 "
+        style={{
+          fontFamily: "font-serif",
+          ":hover": {
+            background:
+              "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
+          },
+        }}
+        href="/homepage"
+      >
+        Home
+      </Link>
 
-    <Link
-      className="text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
-      style={{
-        fontFamily: "font-serif",
-        ":hover": {
-          background:
-            "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
-        },
-      }}
-      href="/about"
-    >
-      About
-    </Link>
+      <Link
+        className="link text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
+        style={{
+          fontFamily: "font-serif",
+          ":hover": {
+            background:
+              "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
+          },
+        }}
+        href="/about"
+      >
+        About
+      </Link>
 
-    <Link
-      className="text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
-      style={{
-        fontFamily: "font-serif",
-        ":hover": {
-          background:
-            "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
-        },
-      }}
-      href="/store"
-    >
-      Store
-    </Link>
+      <Link
+        className="link text-xl hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
+        style={{
+          fontFamily: "font-serif",
+          ":hover": {
+            background:
+              "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
+          },
+        }}
+        href="/store"
+      >
+        Store
+      </Link>
 
-    <Link
-      className="text-xl  hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
-      style={{
-        fontFamily: "font-serif",
-        ":hover": {
-          background:
-            "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
-        },
-      }}
-      href="/tips"
-    >
-      Tips
-    </Link>
-    <Link
-      href="/store/buycart"
-      className="hover:cursor-pointer hover:transform hover:scale-110 transition-transform duration-300"
-    >
-      <div className="flex items-center gap-4">
-        <img
-          src="/images/shoppingCart.png"
-          alt="shoppingCartImage"
-          style={{ width: "25px", height: "25px" }}
-        />
-        <div
-          style={{
-            backgroundColor: "white",
-            color: "green",
-            borderRadius: "100%",
-            width: "20px",
-            textAlign: "center",
-          }}
-        >
-          {totalItems}
+      <Link
+        className="link text-xl  hover:rounded-lg hover:text-black px-2 hover:transform hover:scale-110 transition-transform duration-300"
+        style={{
+          fontFamily: "font-serif",
+          ":hover": {
+            background:
+              "linear-gradient(to right top, #527e7b, #4e8780, #4b9183, #499a84, #4ba384)",
+          },
+        }}
+        href="/tips"
+      >
+        Tips
+      </Link>
+      <Link
+        href="/store/buycart"
+        className="link hover:cursor-pointer hover:transform hover:scale-110 transition-transform duration-300"
+      >
+        <div className="flex items-center gap-4">
+          <img
+            src="/images/shoppingCart.png"
+            alt="shoppingCartImage"
+            style={{ width: "25px", height: "25px" }}
+          />
+          <div
+            style={{
+              backgroundColor: "white",
+              color: "green",
+              borderRadius: "100%",
+              width: "20px",
+              textAlign: "center",
+            }}
+          >
+            {totalItems}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+     </div>
+      
 
-    <div className="mr-8 mt-2">
-      <ButtonAuth />
-    </div>
-  </section>
-);
+      <div className="mr-8 mt-2">
+        <ButtonAuth />
+      </div>
+    </section>
+  )
+};
 
 const NavBar = () => {
   const pathname = usePathname;
   const { data: session } = useSession();
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const { cart } = useContext(CartContext);
 
@@ -140,7 +155,7 @@ const NavBar = () => {
   return (
     <div>
       <nav
-        className="py-3 items-center text-white flex justify-between "
+        className="py-3 items-center text-white flex justify-between navegacion"
         style={{
           background:
             "linear-gradient(to right top, #527e7b, #4a7771, #426f66, #3b685c, #356051)",
@@ -148,7 +163,7 @@ const NavBar = () => {
       >
         <LogoSection />
 
-        <NavigationLinks session={session} totalItems={totalItems} />
+        <NavigationLinks session={session} totalItems={totalItems} isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
       </nav>
     </div>
   );
