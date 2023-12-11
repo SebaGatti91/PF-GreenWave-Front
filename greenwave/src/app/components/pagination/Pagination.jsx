fd649a08ba5e7dcbd198ab80040b0d763c82e03b
ulminadir <ulminadir99@gmail.com>
2023-12-11 14:45:02 -0300

    se modifica estilo de paginacion

--- greenwave/src/app/components/pagination/Pagination.jsx
+++ greenwave/src/app/components/pagination/Pagination.jsx
@@ -21,12 +21,12 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
       pageNumbers.push(
         <button
           key={i}
-          className={`mx-1 px-2 py-1 rounded-lg ${
+          className={` px-2 py-1 border-2 border-lime-100 ${
             i === currentPage
               ? "bg-boton text-white"
-              : "bg-hover text-black  px-2 py-1 rounded-lg text-2xl hover:bg-boton"
+              : "bg-hover text-black  flex items-center justify-center px-4 leading-tight hover:bg-boton"
           }`}
-          style={{ width: "40px", margin: "5px" }}
+          style={{ width: "30px", }}
           onClick={() => {
             onPageChange(i);
             window.scrollTo({ top: 0, behavior: "smooth" });
@@ -81,22 +81,22 @@ const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   // };
 
   return (
-    <div className="flex justify-center py-2 mt-4">
+    <div className="flex justify-center py-2 mt-4 mb-3">
       <button
-        className="bg-hover px-2 py-1 rounded-lg text-2xl hover:bg-boton"
+        className="bg-hover border-2 border-lime-100 flex items-center justify-center px-4 h-10 ms-0 leading-tight hover:bg-boton rounded-l-lg"
         onClick={() => {
           onPageChange(currentPage - 1);
           window.scrollTo({ top: 0, behavior: "smooth" });
         }}
         disabled={currentPage === 1}
       >
-        &#8249; {/* Flecha izquierda */}
+         &#8249; {/* Flecha izquierda */}
       </button>
 
       {renderPageNumbers()}
 
       <button
-        className="bg-hover px-2 py-1 rounded-lg text-2xl hover:bg-boton"
+        className="bg-hover border-2 border-lime-100 flex items-center justify-center px-4 h-10 ms-0 leading-tight hover:bg-boton rounded-r-lg "
         onClick={() => {
           onPageChange(currentPage + 1);
           window.scrollTo({ top: 0, behavior: "smooth" });
