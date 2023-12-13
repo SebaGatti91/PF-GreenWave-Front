106959c6e658e97e3968e5c6a1f943cc307e1a32
SebaGatti <seba.g.1391@gmail.com>
2023-12-13 18:23:05 -0300

    cambios finales en el dashboard

--- greenwave/src/app/components/dashboard/navBar/NavBar.jsx
+++ greenwave/src/app/components/dashboard/navBar/NavBar.jsx
@@ -15,11 +15,11 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.title}>{pathname.split("/").pop()}</div>
       <div className={styles.menu}>
-        <div className={styles.icons}>
+        {/* <div className={styles.icons}>
           <MdOutlineChat size={20} />
           <MdNotifications size={20} />
           <MdPublic size={20} />
-        </div>
+        </div> */}
       </div>
     </div>
   );
