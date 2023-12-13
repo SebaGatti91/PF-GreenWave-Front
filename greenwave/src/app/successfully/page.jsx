35ec839031afc68e3d0f48b0eb0ad6b95cc47bcf
SebaGatti <seba.g.1391@gmail.com>
2023-12-12 23:06:16 -0300

    cambios developer

--- greenwave/src/app/successfully/page.jsx
+++ greenwave/src/app/successfully/page.jsx
@@ -1,26 +1,37 @@
 "use client";
 import Link from "next/link";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect} from "react";
 import { CartContext } from "../components/cart/cartContext";
 import { fetchFeedback } from "../lib/data";
 
 export default function Success() {
+  
   const { cart, setCart } = useContext(CartContext);
 
+  const debounce = (func, wait) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), wait);
+    };
+  };
+  
+  const debouncedFetchFeedback = debounce(fetchFeedback, 500); // Adjust wait time as needed
+  
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchDataAndResetCart = async () => {
       try {
-        await fetchFeedback(cart);
+        if (cart.length > 0) {
+          debouncedFetchFeedback(cart);
+          setCart([]);
+        }
       } catch (error) {
         console.error("Error fetching users:", error);
       }
     };
-
-    
-    fetchData();
-    
-    setCart([]);
-  }, []);
+  
+    fetchDataAndResetCart();
+  }, [cart, setCart])
 
   return (
     <div
@@ -31,9 +42,11 @@ export default function Success() {
         Your purchase has been a success!
       </h1>
       <span className="m-3 block max-w-lg">
+        <p>
         Thank you from the bottom of our hearts for choosing us! Your purchase
         has been a resounding success, and we are thrilled to have you as part
         of our valued customer community.
+        </p>
       </span>
       <div className="m-2">
         <img src="/images/Green-Wave.png" alt="greenWave logo" />
