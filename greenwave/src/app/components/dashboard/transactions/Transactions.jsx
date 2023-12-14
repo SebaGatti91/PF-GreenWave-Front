43a09d907d8cde7ee609b9683b996e678887ea1c
SebaGatti <seba.g.1391@gmail.com>
2023-12-13 23:49:18 -0300

    cambios paginado

--- greenwave/src/app/components/dashboard/transactions/Transactions.jsx
+++ greenwave/src/app/components/dashboard/transactions/Transactions.jsx
@@ -1,14 +1,19 @@
 "use client";
-import Image from "next/image";
-import styles from "./transactions.module.css";
+import { MdSearch } from "react-icons/md";
 import React, { useState, useEffect } from "react";
-import { fetchUsers } from "../../../lib/data";
+import styles from "../../components/dashboard/users/users.module.css";
+import { fetchUsers } from "../../lib/data";
+import Image from "next/image";
 
-const Transactions = () => {
+
+const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 8; // Número de usuarios por página
 
   useEffect(() => {
-    const fetchUsersData = async () => {
+    const fetchData = async () => {
       try {
         if (users.length === 0) {
           const fetchedUsers = await fetchUsers();
@@ -19,52 +24,117 @@ const Transactions = () => {
       }
     };
 
-    fetchUsersData();
+    fetchData();
   }, [users]);
+
+  // Agrega el estado para el término de búsqueda
+  const handleSearch = (user) => {
+    const normalizedSearchTerm = searchTerm.toLowerCase();
+    return (
+      user.email.toLowerCase().includes(normalizedSearchTerm) ||
+      user.id.toString().includes(normalizedSearchTerm) ||
+      user.username.toLowerCase().includes(normalizedSearchTerm)
+    );
+  };
+
+  // Filtra usuarios basado en el término de búsqueda
+  const filteredUsers = users.filter(handleSearch);
+
+  // Actualiza la lógica de paginación
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const paginatedUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+  // Maneja el cambio de página
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Transactions by Users</h2>
+      {/* Barra de búsqueda */}
+      <div className={styles.search}>
+        <MdSearch />
+        <input
+          type="text"
+          placeholder="Search by email or username"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+            setCurrentPage(1); // Restablece la página cuando cambia el término de búsqueda
+          }}
+          className={styles.input}
+        />
+      </div>
+
+      {/* Resto del contenido */}
       <table className={styles.table}>
         <thead>
           <tr>
-            <th>Image</th>
-            <th>Username</th>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Amount</th>
-            <th>Total</th>
+            <td>User</td>
+            <td>Status</td>
+            <td>Username</td>
+            <td>Email</td>
+            <td>Credits</td>
+            <td>Rol</td>
           </tr>
         </thead>
         <tbody>
-          {users?.map((user) =>
-            user.purchases && user.purchases.length > 0
-              ? user.purchases.map((purchase) => (
-                  <tr key={`${user.id}-${purchase.Product.id}`}>
-                    <td>
-                      <div className={styles.user}>
-                        <Image
-                          src={user.image ? user.image : "/images/noavatar.png"}
-                          alt=""
-                          width={40}
-                          height={40}
-                          className={styles.userImage}
-                        />
-                      </div>
-                    </td>
-                    <td>{user.username}</td>
-                    <td>{purchase.Product.name}</td>
-                    <td>{purchase.quantity}</td>
-                    <td>${purchase.Product.price}</td>
-                    <td>${purchase.Product.price * purchase.quantity}</td>
-                  </tr>
-                ))
-              : null
-          )}
+          {paginatedUsers.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.image || "/images/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                </div>
+              </td>
+              <td
+                className={`${styles.status} ${
+                  user.status ? styles.active : styles.banned
+                }`}
+              >
+                {user.status ? "Active" : "Banned"}
+              </td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td>{user.credits}</td>
+              <td
+                className={`${styles.rol} ${
+                  user.admin ? styles.admin : styles.client
+                }`}
+              >
+                {user.admin ? "Admin" : "User"}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
+      {/* Botones de paginación */}
+      <div>
+        <button
+          className={styles.buttonBottom}
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Anterior
+        </button>
+        <span>Página {currentPage}</span>
+        <button
+          className={styles.buttonBottom}
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={indexOfLastUser >= filteredUsers.length}
+        >
+          Siguiente
+        </button>
+      </div>
     </div>
   );
 };
 
-export default Transactions;
+export default UsersPage;
