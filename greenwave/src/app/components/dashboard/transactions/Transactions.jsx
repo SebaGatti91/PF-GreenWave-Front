deea7adc6dc53ae4dedfd5ceee06472c7eab3767
SebaGatti <seba.g.1391@gmail.com>
2023-12-14 01:10:33 -0300

    cambios paginacion dashboard

--- greenwave/src/app/components/dashboard/transactions/Transactions.jsx
+++ greenwave/src/app/components/dashboard/transactions/Transactions.jsx
@@ -1,124 +1,108 @@
 "use client";
-import Image from "next/image";
-import styles from "./transactions.module.css";
-import React, { useState, useEffect } from "react";
-import { fetchUsers } from "../../../lib/data";
 import { MdSearch } from "react-icons/md";
+import React, { useState, useEffect } from "react";
+import styles from "../../components/dashboard/users/users.module.css";
+import { fetchUsers } from "../../lib/data";
+import Image from "next/image";
+import Link from "next/link";
 
-const Transactions = () => {
+const UsersPage = () => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
-  const usersPerPage = 5;
 
   useEffect(() => {
-    const fetchUsersData = async () => {
+    const fetchData = async () => {
       try {
-        const fetchedUsers = await fetchUsers();
-        setUsers(fetchedUsers);
+        if (users.length === 0) {
+          const fetchedUsers = await fetchUsers();
+          setUsers(fetchedUsers);
+        }
       } catch (error) {
         console.error("Error fetching users:", error);
       }
     };
 
-    fetchUsersData();
-  }, []); // Solo se ejecuta una vez al montar la página
-
-  // Cambiar de página
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
-  };
+    fetchData();
+  }, [users]);
 
   const handleSearch = (user) => {
     const normalizedSearchTerm = searchTerm.toLowerCase();
     return (
       user.email.toLowerCase().includes(normalizedSearchTerm) ||
-      user.id.toString().includes(normalizedSearchTerm) ||
-      user.username.toLowerCase().includes(normalizedSearchTerm)
+      user.id.toString().includes(normalizedSearchTerm)
     );
   };
 
-  // Filtrar usuarios basados en el término de búsqueda
+  // Filter users based on search term
   const filteredUsers = users.filter(handleSearch);
-
-  // Lógica de paginación
-  const indexOfLastUser = currentPage * usersPerPage;
-  const indexOfFirstUser = indexOfLastUser - usersPerPage;
-  const paginatedUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Transactions by Users</h2>
       <div className={styles.search}>
         <MdSearch />
         <input
           type="text"
-          placeholder="Search by email or username"
+          placeholder="Search by email "
           value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-            setCurrentPage(1); // Reinicia la página cuando cambia el término de búsqueda
-          }}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className={styles.input}
         />
       </div>
       <table className={styles.table}>
         <thead>
           <tr>
-            <th>Image</th>
-            <th>Username</th>
-            <th>Product</th>
-            <th>Quantity</th>
-            <th>Amount</th>
-            <th>Total</th>
+            <td className={styles.columnHeader}>User</td>
+            <td className={styles.columnHeader}>Status</td>
+            <td className={styles.columnHeader}>Username</td>
+            <td className={styles.columnHeader}>Email</td>
+            <td className={styles.columnHeader}>Rol</td>
           </tr>
         </thead>
         <tbody>
-          {paginatedUsers.map((user) =>
-            user.purchases && user.purchases.length > 0
-              ? user.purchases.map((purchase) => (
-                  <tr key={`${user.id}-${purchase.Product.id}`}>
-                    <td>
-                      <div className={styles.user}>
-                        <Image
-                          src={user.image ? user.image : "/images/noavatar.png"}
-                          alt=""
-                          width={40}
-                          height={40}
-                          className={styles.userImage}
-                        />
-                      </div>
-                    </td>
-                    <td>{user.username}</td>
-                    <td>{purchase.Product.name}</td>
-                    <td>{purchase.quantity}</td>
-                    <td>${purchase.Product.price}</td>
-                    <td>${purchase.Product.price * purchase.quantity}</td>
-                  </tr>
-                ))
-              : null
-          )}
+          {filteredUsers.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.image || "/images/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                </div>
+              </td>
+              <td
+                className={`${styles.status} ${
+                  user.status ? styles.active : styles.banned
+                }`}
+              >
+                {user.status ? "Active" : "Banned"}
+              </td>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+              <td
+                className={`${styles.rol} ${
+                  user.admin ? styles.admin : styles.client
+                }`}
+              >
+                {user.admin ? "Admin" : "User"}
+              </td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
-      <div>
-        <button
-          className={styles.buttonBottom}
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          Anterior
-        </button>
-        <span>Página {currentPage}</span>
-        <button
-          className={styles.buttonBottom}
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={indexOfLastUser >= filteredUsers.length}
-        >
-          Siguiente
-        </button>
-      </div>
     </div>
   );
 };
 
-export default Transactions;
+export default UsersPage;
